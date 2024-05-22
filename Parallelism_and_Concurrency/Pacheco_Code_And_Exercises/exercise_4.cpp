dda975042cd1974f69a4c493c5da69ec732d45f2
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2024-05-21 22:55:35 -0300

    UPDATE: Exercise 4. ADD: Exercise 9

--- Parallelism_and_Concurrency/Pacheco_Code_And_Exercises/exercise_4.cpp
+++ Parallelism_and_Concurrency/Pacheco_Code_And_Exercises/exercise_4.cpp
@@ -1,13 +1,42 @@
 #include <iostream>
 #include <mpi.h>
+#include <string>
+#include <sstream>
+
+// 3.4 Modify the program that just prints a line of output from each process
+// (mpi_output.c) so that the output is printed in process rank order: process 0’s
+// output first, then process 1’s, and so on.
+
+/// ANSWER: check code
 
  int main( ){
 
-    int my_rank{ 0 }, comm_sz{ 0 };
+   int my_rank{ 0 }, comm_sz{ 0 };
 
-    MPI_Init( nullptr, nullptr );
-    MPI_Comm_rank( MPI_COMM_WORLD,  &my_rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &comm_sz );
+   MPI_Init( nullptr, nullptr );
+   MPI_Comm_rank( MPI_COMM_WORLD,  &my_rank );
+   MPI_Comm_size( MPI_COMM_WORLD, &comm_sz );
 
-    MPI_Finalize( );
+   if( my_rank != 0 ){
+
+      std::string message{ ( std::ostringstream( ) << "Process " << my_rank << " of " <<  comm_sz << " > Does anyone have a toothpick?\n" ).str( ) };
+
+      MPI_Send( message.data( ), message.size( ), MPI_CHAR, 0, 0, MPI_COMM_WORLD );
+   }
+
+   else{
+
+      std::string message{ ( std::ostringstream( ) << "Process " << my_rank << " of " <<  comm_sz << " > Does anyone have a toothpick?\n" ).str( ) };
+
+      printf( message.data( ) );
+
+      for( int process = 1; process < comm_sz; ++process ){
+
+         MPI_Recv( message.data( ), message.size( ), MPI_CHAR, process, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE );
+
+         printf( message.data( ) );
+      }
+   }
+
+   MPI_Finalize( );
  }
---
+++ Parallelism_and_Concurrency/Pacheco_Code_And_Exercises/exercise_9.cpp
@@ -0,0 +1,112 @@
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include <random>
+
+// 3.9 Write an MPI program that implements multiplication of a vector by a scalar
+// and dot product. The user should enter two vectors and a scalar, all of which
+// are read in by process 0 and distributed among the processes. The results are
+// calculated and collected onto process 0, which prints them. You can assume
+// that n, the order of the vectors, is evenly divisible by comm_sz.
+
+/// ANSWER: to make testing faster, I am not implementing user input. Instead, I use
+///         a RNG to fill the vectors. The user only sets the size of the vectors and
+///         the scalar.
+
+void program_input( double& scalar, int& size ){
+
+    printf( "Enter a scalar and the vectors' size: " );
+    scanf( "%lf %d", &scalar, &size );
+}
+
+void mult_vector_scalar( double scalar, std::vector<double>& vec ){
+
+    for( std::vector<double>::size_type i = 0; i < vec.size( ); ++i ){
+
+        vec[ i ] *= scalar;
+    }
+}
+
+double dot_product( std::vector<double>& left, std::vector<double>& right ){
+
+    double sum{ 0 };
+
+    for( std::vector<double>::size_type i = 0; i < left.size( ); ++i ){
+
+        sum += left[ i ] * right[ i ];
+    }
+
+    return sum;
+}
+
+void create_shared_data( std::vector<double>& left, std::vector<double>& right ){
+
+    double scalar{ 0 };
+    int size{ 0 };
+
+    program_input( scalar, size );
+
+    left.resize( size );
+    right.resize( size );
+
+    std::random_device left_rand, right_rand;
+    std:: uniform_real_distribution<double> dist( -size, size );
+    std::mt19937_64 left_rng( left_rand( ) ), right_rng( right_rand( ) );
+
+     for( std::vector<double>::size_type i = 0; i < size; ++i ){
+
+        left[ i ] = dist( left_rng );
+        right[ i ] = dist( right_rng );
+     }
+}
+
+void share_data( int my_rank, int comm_sz, std::vector<double>& left, std::vector<double>& right, MPI_Comm comm ){
+
+    std::vector<double> holder_l{ }, holder_r{ };
+
+    int buffsize{ 0 };
+
+    if( my_rank == 0 ){
+
+        create_shared_data( holder_l, holder_r );
+
+        buffsize = ( holder_l.size( ) / comm_sz ) + ( holder_l.size( ) % comm_sz );
+        
+        left.resize( buffsize );
+        right.resize( buffsize );
+
+        MPI_Bcast( &buffsize, 1, MPI_INT, 0, comm );
+
+        MPI_Scatter( holder_l.data( ), buffsize, MPI_DOUBLE, left.data( ), buffsize, MPI_DOUBLE, 0, comm );
+        MPI_Scatter( holder_r.data( ), buffsize, MPI_DOUBLE, right.data( ), buffsize, MPI_DOUBLE, 0, comm );
+
+        holder_l.clear( );
+        holder_r.clear( );
+    }
+
+    else{
+
+        MPI_Bcast( &buffsize, 1, MPI_INT, 0, comm );
+
+        left.resize( buffsize );
+        right.resize( buffsize );
+
+        MPI_Scatter( holder_l.data( ), buffsize, MPI_DOUBLE, left.data( ), buffsize, MPI_DOUBLE, 0, comm );
+        MPI_Scatter( holder_r.data( ), buffsize, MPI_DOUBLE, right.data( ), buffsize, MPI_DOUBLE, 0, comm );
+    }
+}
+
+int main( ){
+
+    int comm_sz{ 0 }, my_rank{ 0 };
+
+    MPI_Init( nullptr, nullptr );
+    MPI_Comm_size( MPI_COMM_WORLD, &comm_sz );
+    MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
+
+    std::vector<double> left{ }, right{ };
+    
+    share_data( my_rank, comm_sz, left, right, MPI_COMM_WORLD );
+    
+    MPI_Finalize( );
+}
