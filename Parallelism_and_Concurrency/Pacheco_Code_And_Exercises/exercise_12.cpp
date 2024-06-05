0d734bef66fc93608e66a93b71773ef9bf45095a
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2024-06-04 22:39:11 -0300

    UPDATE: minor update Exercise 12

--- Parallelism_and_Concurrency/Pacheco_Code_And_Exercises/exercise_12.cpp
+++ Parallelism_and_Concurrency/Pacheco_Code_And_Exercises/exercise_12.cpp
@@ -20,9 +20,19 @@ are p processes, each process q sends data to process q + 1, except that process
 //  a.  Write an MPI program that implements this algorithm for allreduce. How does its performance compare to the
 //      butterfly-structured allreduce?
 
-void MPI_Ring_Allreduce( int my_rank, int comm_size ){
+void MPI_Ring_Allreduce( int my_rank, int comm_size, int my_val, int& result, MPI_Comm comm ){
 
-    
+    int sum{ my_val }, temp_val{ my_val };
+
+    for( auto i = 1; i < comm_size; ++i ){
+
+        // This is wrong: whenever i + 1 > comm_size, this modulo will return an invalid receiver index
+        MPI_Sendrecv_replace( &temp_val, 1, MPI_INT, ( ( i + 1 ) % comm_size ), 0, i, 0, comm, MPI_STATUS_IGNORE  );
+
+        sum += temp_val;
+    }
+
+    result = sum;
 }
 
 // b.  Modify the MPI program you wrote in the first part so that it implements prefix sums.
@@ -35,6 +45,17 @@ int main( ){
     MPI_Init( nullptr, nullptr );
     MPI_Comm_rank( MPI_COMM_WORLD, &my_rank );
     MPI_Comm_size( MPI_COMM_WORLD, &comm_size );
+
+    int result{ 0 };
+
+    MPI_Ring_Allreduce( my_rank, comm_size, my_rank, result, MPI_COMM_WORLD );
+
+    if( my_rank == 0 ){
+
+        printf( "%d", result );
+
+        printf( "\n" );
+    }
 
     MPI_Finalize( );
 }
