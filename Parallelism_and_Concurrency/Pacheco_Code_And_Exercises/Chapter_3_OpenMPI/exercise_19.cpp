70bebe8e3f5fc8aa42d210463d536a7431934e4a
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2024-06-20 21:48:30 -0400

    UPDATE: Exercise 19 - END

--- Parallelism_and_Concurrency/Pacheco_Code_And_Exercises/Chapter_3_OpenMPI/exercise_19.cpp
+++ Parallelism_and_Concurrency/Pacheco_Code_And_Exercises/Chapter_3_OpenMPI/exercise_19.cpp
@@ -14,28 +14,93 @@
 // upper triangular part with a single call to MPI_Send. Process 1 should receive the upper triangular part with
 // a single call to MPI_Recv, and then print the data it received.
 
-void Read_Matrix( int row, MPI_Datatype type, MPI_Comm comm ){
+std::vector<double> Build_Matrix( int row ){
 
     std::random_device device;
-    std::uniform_real_distribution<double> dist( -row, row );
+    std::uniform_real_distribution<double> dist( -( row * row ), ( row * row ) );
     std::mt19937_64 rng( device( ) );
 
-    std::vector<std::vector<double>> matrix( row, std::vector<double>( row ) );
+    std::vector<double> matrix( row * row );
 
     for( auto i = 0; i < matrix.size( ); ++i ){
-
-        for( auto j = 0; j < matrix[ i ].size( ); ++j ){
-
-            matrix[ i ][ j ] = dist( rng );
-        }
+    
+        matrix[ i ] = dist( rng );    
     }
 
-    
+    return matrix;
 }
 
-void Print_Data( MPI_Datatype type, MPI_Comm comm ){
+std::vector<int> Block_length( int row ){
 
+    std::vector<int> Blocks( row );
 
+    for( auto i = 0; i < Blocks.size( ); ++i ){
+
+        Blocks[ i ] = row - i;
+    }
+
+    return Blocks;
+}
+
+int count( std::vector<int>& blocks ){
+
+    int elements{ 0 };
+
+    for( auto i = 0; i < blocks.size( ); ++i ){
+
+        elements += blocks[ i ];
+    }
+
+    return elements;
+}
+
+/// NOTE: the amount of displacements MUST match the amount of blocks. Even if it happens
+///       that one displacement == 0!!!!!!!
+std::vector<int> Displacements( int row ){
+
+    std::vector<int> result{ };
+
+    for( auto i = 0; i < row; ++i ){
+
+        //result.push_back( ( row * i ) + i );
+        result.push_back( i );
+    }
+
+    return result;
+}
+
+void Read_Matrix( int row, MPI_Comm comm ){
+
+    std::vector<double> matrix = Build_Matrix( row );
+    std::vector<int> blocks = Block_length( row );
+    std::vector<int> displace = Displacements( row ); // CHECK NOTE ON DISPLACEMENTS
+    int total = count( blocks );
+
+    MPI_Datatype type;
+    MPI_Type_indexed( row, blocks.data( ), displace.data( ), MPI_DOUBLE, &type ); // // CHECK NOTE ON DISPLACEMENTS
+    MPI_Type_commit( &type );
+
+    MPI_Send( &total, 1, MPI_INT, 1, 0, comm );
+    MPI_Send( matrix.data( ), 1, type, 1, 0, comm );
+
+    MPI_Type_free( &type );
+}
+
+void Print_Data( MPI_Comm comm ){
+
+    int amount{ 0 };
+    MPI_Recv( &amount, 1, MPI_INT, 0, 0, comm, MPI_STATUS_IGNORE );
+
+    std::vector<double> output( amount );
+
+    MPI_Recv( output.data( ), output.size( ), MPI_DOUBLE, 0, 0, comm, MPI_STATUS_IGNORE );
+
+    for( auto i = 0; i < output.size( ); ++i ){
+
+        std::cout << output[ i ] << " ";
+    }
+
+    printf( "\n" );
 }
 
 int main( ){
@@ -45,6 +110,21 @@ int main( ){
     MPI_Init( nullptr, nullptr );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &comm_size );
+
+    if( rank == 0 ){
+
+        int size{ 0 };
+
+        printf( "Enter the dimensions of the matrix: \n" );
+        scanf( "%d", &size );
+
+        Read_Matrix( size, MPI_COMM_WORLD );
+    }
+
+    else{
+
+        Print_Data( MPI_COMM_WORLD );
+    }
 
     MPI_Finalize( );
 }
