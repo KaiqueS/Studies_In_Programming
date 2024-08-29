ca397ea611025c1211bc7c8067e391c06fb198b4
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2024-08-28 22:32:31 -0300

    UPDATE: Exercise 1

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter2/exercise_1.cu
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter2/exercise_1.cu
@@ -3,14 +3,31 @@
 #include <iostream>
 #include <random>
 
+void fill_matrix( float*& matrix, int size ){
+
+    std::random_device dev;
+    std::uniform_real_distribution<float> dist( -( size * size ), ( size * size ) );
+    std::mt19937_64 rng( dev( ) );
+
+    int dim = size * size;
+
+    matrix = new float( dim );
+
+    for( auto i = 0; i < dim; ++i ){
+
+        matrix[ i ] = dist( rng );
+    }
+}
+
 /// 1. A matrix addition takes two input matrices A and B and produces one output matrix C. Each element of the output matrix C is the sum of the corresponding
 /// elements of the input matrices A and B, i.e., C[i][j] = A[i][j] + B[i][j]. For simplicity, we will only handle square matrices whose elements are
 /// single-precision floating-point numbers. Write a matrix addition kernel and the host stub function that can be called with four parameters: pointer-
 /// to-the-output matrix, pointer-to-the-first-input matrix, pointer-to-the-second-input matrix, and the number of elements in each dimension. Follow the
 /// instructions below:
 
-// B. Write a kernel that has each thread to produce one output matrix element. Fill in the execution configuration parameters for this design.
+/// B. Write a kernel that has each thread to produce one output matrix element. Fill in the execution configuration parameters for this design.
 
+// Problem: how to generalize for instances where the dimensions of the matrix are greater than the available amount of threads?
 __global__
 void matrixAddKernel_B( float* output, float* first_input, float* second_input, int size ){
 
@@ -47,7 +64,7 @@ void matrixAddKernel_D( float* output, float* first_input, float* second_input, 
 
 void set_up( float* h_output, float* h_first_input, float* h_second_input, int dim ){
 
-    float *d_Output, *d_first, *d_second;
+    float *d_Output{ nullptr }, *d_first{ nullptr }, *d_second{ nullptr };
     
     int size = dim * sizeof( float );
 
@@ -58,7 +75,15 @@ void set_up( float* h_output, float* h_first_input, float* h_second_input, int d
     cudaMemcpy( d_first, h_first_input, size, cudaMemcpyHostToDevice );
     cudaMemcpy( d_second, h_second_input, size, cudaMemcpyHostToDevice );
 
-    matrixAddKernel_B<<<32,32>>>( d_Output, d_first, d_second, dim );
+    int blocks{ 0 }, threads{ 0 };
+
+    printf( "Enter the Grid and Block sizes: \n" );
+    std::cin >> blocks >> threads;
+
+    dim3 dimGrid( blocks, 1, 1 );
+    dim3 dimBlock( threads, 1, 1 );
+
+    matrixAddKernel_B<<<dimGrid, dimBlock>>>( d_Output, d_first, d_second, dim ); // 32,32 are just dummy inputs, else the code would not compile.
 
     cudaMemcpy( h_output, d_Output, size, cudaMemcpyDeviceToHost );
 
@@ -69,5 +94,22 @@ void set_up( float* h_output, float* h_first_input, float* h_second_input, int d
 
 int main( ){
 
+    float *output{ nullptr }, *first{ nullptr }, *second{ nullptr };
 
+    int dimension{ 0 };
+
+    std::cout << "Enter the matrix dimensions:" << "\n";
+    std::cin >> dimension;
+
+    fill_matrix( first, dimension );
+    fill_matrix( second, dimension );
+
+    for( auto i = 0; i < ( dimension * dimension ); ++i ){
+
+        printf( "%lf ", first[ i ] );
+    }
+
+    std::cout << "\n";
+
+    delete[ ] first, second;
 }
