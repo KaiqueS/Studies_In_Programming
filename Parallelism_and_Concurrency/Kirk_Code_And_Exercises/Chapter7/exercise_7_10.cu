b3b7ae2af1af3c0731fe95489dc2c93499d96c53
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2025-03-07 23:11:31 -0300

    UPDATE: Exercise 7.10 - started over

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter7/exercise_7_10.cu
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter7/exercise_7_10.cu
@@ -146,6 +146,7 @@ __constant__ double filter[ ( 2 * FILTER_RADIUS ) + 1 ][ ( 2 * FILTER_RADIUS ) +
 
 /*__global__ void constM_tiled_convolution_3d( double* input_matrix, double* output_matrix, int height, int width, int depth, int shared_memsize ){
 
+	// Output matrix slice, row, and column
 	int slice = ( blockIdx.z * OUT_TILE_DIM ) + threadIdx.z - FILTER_RADIUS;
 	int row = ( blockIdx.y * OUT_TILE_DIM ) + threadIdx.y - FILTER_RADIUS;
 	int col = ( blockIdx.x * OUT_TILE_DIM ) + threadIdx.x - FILTER_RADIUS;
@@ -201,41 +202,73 @@ __constant__ double filter[ ( 2 * FILTER_RADIUS ) + 1 ][ ( 2 * FILTER_RADIUS ) +
 
 }*/
 
-__global__ void constM_tiled_convolution_3d( double* input_matrix, double* output_matrix, int height, int width, int depth ){
+__global__ void constM_tiled_convolution_3d( double* input_matrix, double* output_matrix, int height, int width, int depth, int radius ){
 
-	const int out_tile_dim = blockDim.z - ( 2 * FILTER_RADIUS );
+	const int out_tile_dim = blockDim.z - ( 2 * radius );
 
-	// Block dimensions matches input tile + radius BEFORE this function is called
+	// Blocks' dimensions matches input tile + radius BEFORE this function is called
 	int in_tile_slice = ( blockDim.z * blockIdx.z ) + threadIdx.z;
 	int in_tile_row = ( blockDim.y * blockIdx.y ) + threadIdx.y;
 	int in_tile_col = ( blockDim.x * blockIdx.x ) + threadIdx.x;
 
-	// Output matrix is in Global Memory
-	int output_slice = ( ( blockDim.z - FILTER_RADIUS ) * blockIdx.z ) + threadIdx.z + FILTER_RADIUS;
-	int output_row = ( ( blockDim.y - FILTER_RADIUS ) * blockIdx.y ) + threadIdx.y + FILTER_RADIUS;
-	int output_col = ( ( blockDim.x - FILTER_RADIUS ) * blockIdx.x ) + threadIdx.x + FILTER_RADIUS;
+	// Output matrix is in Global Memory, which means that, if we use multiple blocks, we must use blockDim and blockId to be able to assign threads from different
+	// blocks to their corresponding tiles on the output matrix.
+	// blockDim - FILTER_RADIUS = output tile dimensions
+	// Subtracting FILTER_RADIUS at the end of the expression( after threadIdx ) messes up the following conditional, because output_slice/row/col that should have been greater than 0 are actually equal to it
+	int output_slice = ( ( blockDim.z - radius ) * blockIdx.z ) + threadIdx.z - radius;
+	int output_row = ( ( blockDim.y - radius ) * blockIdx.y ) + threadIdx.y - radius;
+	int output_col = ( ( blockDim.x - radius ) * blockIdx.x ) + threadIdx.x - radius;
 
 	// Shared memory allocation matches block dimensions by definition, i.e., at the call of this function
 	__shared__ extern double shared_input[];
 
 	// Loading into shared memory
-	if( in_tile_slice >= 0 && in_tile_slice < depth && in_tile_row >= 0 && in_tile_row < height && in_tile_col >= 0 && in_tile_col < width ){
+	if( output_slice >= 0 && output_slice < depth && output_row >= 0 && output_row < height && output_col >= 0 && output_col < width ){
 
 		// By assumption, input and output matrices have the SAME dimensions
 		// ATTENTION: shared_input is a TILE, i.e., it is not equal to the entirety of the input matrix.
-		shared_input[ ( in_tile_slice * ( blockDim.z * blockDim.z ) ) + ( in_tile_row * ( blockDim.y - FILTER_RADIUS ) ) + ( blockDim.x - FILTER_RADIUS ) ] = input_matrix[ ( output_slice * depth * depth ) + ( output_row * height ) + output_col ];
+		//shared_input[ ( in_tile_slice * ( blockDim.z * blockDim.z ) ) + ( in_tile_row * ( blockDim.y - FILTER_RADIUS ) ) + ( blockDim.x - FILTER_RADIUS ) ] = input_matrix[ ( output_slice * depth * depth ) + ( output_row * height ) + output_col ];
+		shared_input[ ( threadIdx.z * ( blockDim.z * blockDim.z ) ) + ( threadIdx.y * blockDim.y ) + threadIdx.x ] = input_matrix[ ( output_slice * depth * depth ) + ( output_row * height ) + output_col ];
 	}
 
 	else{
 
-		shared_input[ ( in_tile_slice * ( blockDim.z * blockDim.z ) ) + ( in_tile_row * ( blockDim.y - FILTER_RADIUS ) ) + ( blockDim.x - FILTER_RADIUS ) ] = 0.0;
+		//shared_input[ ( in_tile_slice * ( blockDim.z * blockDim.z ) ) + ( in_tile_row * ( blockDim.y - FILTER_RADIUS ) ) + ( blockDim.x - FILTER_RADIUS ) ] = 0.0;
+		shared_input[ ( threadIdx.z * ( blockDim.z * blockDim.z ) ) + ( threadIdx.y * blockDim.y ) + threadIdx.x ] = 0.0;
 	}
 
 	__syncthreads( );
 
-	if( in_tile_slice >= 0 && in_tile_slice < depth && in_tile_row >= 0 && in_tile_row < height && in_tile_col >= 0 && in_tile_col < width ){
+	// PROBLEM: this should not be working when ( blockdim - radius ) < input/output_matrix size
+	if( output_slice >= 0 && output_slice < depth && output_row >= 0 && output_row < height && output_col >= 0 && output_col < width ){
 
-		output_matrix[ ( output_slice * depth * depth ) + ( output_row * height ) + output_col ] = shared_input[ ( in_tile_slice * ( blockDim.z * blockDim.z ) ) + ( in_tile_row * ( blockDim.y - FILTER_RADIUS ) ) + ( blockDim.x - FILTER_RADIUS ) ];
+		//output_matrix[ ( output_slice * depth * depth ) + ( output_row * height ) + output_col ] = shared_input[ ( in_tile_slice * ( blockDim.z * blockDim.z ) ) + ( in_tile_row * ( blockDim.y - FILTER_RADIUS ) ) + ( blockDim.x - FILTER_RADIUS ) ];
+		output_matrix[ ( output_slice * depth * depth ) + ( output_row * height ) + output_col ] = shared_input[ ( threadIdx.z * ( blockDim.z * blockDim.z ) ) + ( threadIdx.y * blockDim.y ) + threadIdx.x ];
+	}
+	
+	int intile_slice = threadIdx.z - radius;
+	int intile_row = threadIdx.y - radius;
+	int intile_col = threadIdx.x - radius;
+
+	if( output_slice >= 0 && output_slice < depth && output_row >= 0 && output_row < height && output_col >= 0 && output_col < width ){
+
+		if( true ){
+
+			double Pvalue{ 0.0 };
+
+			for( int fSlice = 0; fSlice < ( 2 * radius ) + 1; ++fSlice ){
+
+				for( int fRow = 0; fRow < ( 2 * radius ) + 1; ++fRow ){
+
+					for( int fCol = 0; fCol < ( 2 * radius ) + 1; ++fCol ){
+
+						Pvalue += filter[ fSlice ][ fRow ][ fCol ] * shared_input[ ( 0 ) ];
+					}
+				}
+			}
+
+			//output_matrix[ ( output_slice * depth * depth ) + ( output_row * height ) + output_col ] += Pvalue;
+		}
 	}
 }
 
@@ -282,10 +315,12 @@ __global__ void constM_tiled_convolution_3d( double* input_matrix, double* outpu
 
 void set_up( double*& host_InMatrix, double*& host_filter, double*& host_OutMatrix, int host_matrix_height, int host_matrix_width, int host_matrix_depth ){
 
+	int radius{ 1 };
+
 	double* dev_InMatrix{ nullptr }, *dev_OutMatrix{ nullptr };
 
 	long int matrix_dimensions = host_matrix_depth * host_matrix_width * host_matrix_height * sizeof( double );
-	long int filter_dimensions = ( ( 2 * FILTER_RADIUS ) + 1 ) * ( ( 2 * FILTER_RADIUS ) + 1 ) * ( ( 2 * FILTER_RADIUS ) + 1 ) * sizeof( double );
+	long int filter_dimensions = ( ( 2 * radius ) + 1 ) * ( ( 2 * radius ) + 1 ) * ( ( 2 * radius ) + 1 ) * sizeof( double );
 
 	cudaMalloc( ( void** ) &dev_InMatrix, matrix_dimensions );
 	cudaMalloc( ( void** ) &dev_OutMatrix, matrix_dimensions );
@@ -299,7 +334,7 @@ void set_up( double*& host_InMatrix, double*& host_filter, double*& host_OutMatr
 	std::cout << "Enter the amount of blocks and threads: ";
 	std::cin >> num_blocks >> num_threads;
 
-	num_threads += FILTER_RADIUS;
+	num_threads += radius;
 
 	dim3 grid( num_blocks, 1, 1 );
 	dim3 block( num_threads, num_threads, num_threads );
@@ -313,7 +348,7 @@ void set_up( double*& host_InMatrix, double*& host_filter, double*& host_OutMatr
 
 	//convolution_3D<<<grid, block>>>( dev_InMatrix, dev_filter, dev_OutMatrix, radius, host_matrix_height, host_matrix_width, host_matrix_depth );
 	//constM_tiled_convolution_3d<<<grid, block, shared_memsize>>>( dev_InMatrix, dev_OutMatrix, host_matrix_height, host_matrix_width, host_matrix_depth );
-	constM_tiled_convolution_3d<<<grid, block, shared_memsize>>>( dev_InMatrix, dev_OutMatrix, host_matrix_height, host_matrix_width, host_matrix_depth );
+	constM_tiled_convolution_3d<<<grid, block, shared_memsize>>>( dev_InMatrix, dev_OutMatrix, host_matrix_height, host_matrix_width, host_matrix_depth, radius );
 
 	cudaMemcpy( host_OutMatrix, dev_OutMatrix, matrix_dimensions, cudaMemcpyDeviceToHost );
 
