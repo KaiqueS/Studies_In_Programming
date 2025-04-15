b8cceac1160c08815c55187d7f9476582bb1f82d
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2025-04-15 01:12:48 -0300

    ADD: Exercise 10.4 - START

---
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter10/exercise_10_4/exercise_10_4.cu
@@ -0,0 +1,124 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <random>
+
+int*&& generate_array( int size ){
+
+	std::random_device dev;
+	std::uniform_int_distribution<int> dist( -( size * size ), ( size * size ) );
+	std::mt19937_64 rng( dev( ) );
+
+	int* array = new int[ size ];
+
+	for( auto i = 0; i < size; ++i ){
+
+		array[ i ] = dist( rng );
+	}
+
+	return std::move( array );
+}
+
+void print( int*& array, int size ){
+
+	for( auto i = 0; i < size; ++i ){
+
+		std::cout << array[ i ] << " ";
+	}
+
+	std::cout << "\n";
+}
+
+/// PROBLEM: 4. Modify the kernel in Fig. 10.15 to perform a max reduction instead of a sum reduction.
+
+/// ANSWER: 
+
+__global__ void CoarsenedSumReductionKernel( int* input, int* output, unsigned int Coarse_Factor ){
+
+	__shared__ extern float shared_input[ ]; // Shared-Memory size := block dimension BY CONSTRUCTION at kernel call through execution configuration parameters
+
+	unsigned int segment = Coarse_Factor * 2 * blockDim.x * blockIdx.x;
+	unsigned int i = segment + threadIdx.x;
+	unsigned int t = threadIdx.x;
+
+	int greatest = input[ i ];
+
+	for( unsigned int tile = 1; tile < ( Coarse_Factor * 2 ); ++tile ){
+
+		greatest = ( input[ i ] > input[ ( tile * blockDim.x ) + i ] ) ? input[ i ] : input[ ( tile * blockDim.x ) + i ];
+	}
+
+	shared_input[ t ] = greatest;
+	
+	for( unsigned int stride = ( blockDim.x / 2 ); stride >= 1; stride /= 2 ){
+
+		__syncthreads( );
+
+		if( t < stride ){
+
+			shared_input[ t ] = ( shared_input[ t ] > shared_input[ t + stride ] ) ? shared_input[ t ] : shared_input[ t + stride ];
+		}
+	}
+
+	if( t == 0 ){
+
+		atomicMax( output, shared_input[ 0 ] );
+	}
+}
+
+void kernel_setup( int*& host_array, int& output, int size ){
+
+	int* dev_array{ nullptr }, *dev_out{ nullptr };
+
+	int array_size = size * sizeof( int );
+
+	cudaMalloc( ( void** ) &dev_array, size );
+	cudaMalloc( ( void** ) &dev_out, sizeof( int ) );
+
+	cudaMemcpy( dev_array, host_array, array_size, cudaMemcpyHostToDevice );
+	cudaMemcpy( dev_out, &output, sizeof( int ), cudaMemcpyHostToDevice );
+
+	dim3 blocks{ 1, 1, 1 };
+
+	unsigned int coarse_factor{ 0 };
+
+	std::cout << "\nEnter the Coarse Factor: ";
+	std::cin >> coarse_factor;
+
+	unsigned int num_threads{ static_cast<unsigned int>( size ) / coarse_factor };
+
+	dim3 threads{ num_threads, 1, 1 };
+
+	int shared_memsize = size * sizeof( int );
+
+	CoarsenedSumReductionKernel<<<blocks, num_threads, shared_memsize>>>( dev_array, dev_out, coarse_factor );
+
+	cudaMemcpy( &output, dev_out, sizeof( int ), cudaMemcpyDeviceToHost );
+
+	cudaFree( dev_array );
+	cudaFree( dev_out );
+}
+
+int main( ){
+
+	int size{ 0 };
+
+	std::cout << "Enter the size of the array: ";
+	std::cin >> size;
+
+	std::cout << "\n";
+
+	int* array = generate_array( size );
+	int output{ 0 };
+
+	print( array, size );
+
+	kernel_setup( array, output, size );
+
+	std::cout << "\n" << output << "\n";
+
+	delete[ ] array;
+}
