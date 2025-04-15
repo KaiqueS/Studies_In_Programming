01aa302228e77c108843cc78bb36191d97ab9ad0
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2025-04-15 19:20:57 -0300

    UPDATE: Exercise 10.4 - END.

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter10/exercise_10_4/exercise_10_4.cu
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter10/exercise_10_4/exercise_10_4.cu
@@ -38,9 +38,9 @@ void print( int*& array, int size ){
 
 __global__ void CoarsenedSumReductionKernel( int* input, int* output, unsigned int Coarse_Factor ){
 
-	__shared__ extern float shared_input[ ]; // Shared-Memory size := block dimension BY CONSTRUCTION at kernel call through execution configuration parameters
+	__shared__ extern int shared_input[ ]; // Shared-Memory size := block dimension BY CONSTRUCTION at kernel call through execution configuration parameters
 
-	unsigned int segment = Coarse_Factor * 2 * blockDim.x * blockIdx.x;
+	unsigned int segment = Coarse_Factor * 2 * blockDim.x * blockIdx.x; // NOTE: multiply by 2 because we are performing a binary operation, i.e., requires 2 elements
 	unsigned int i = segment + threadIdx.x;
 	unsigned int t = threadIdx.x;
 
@@ -48,7 +48,7 @@ __global__ void CoarsenedSumReductionKernel( int* input, int* output, unsigned i
 
 	for( unsigned int tile = 1; tile < ( Coarse_Factor * 2 ); ++tile ){
 
-		greatest = ( input[ i ] > input[ ( tile * blockDim.x ) + i ] ) ? input[ i ] : input[ ( tile * blockDim.x ) + i ];
+		greatest = ( greatest > input[ ( tile * blockDim.x ) + i ] ) ? greatest : input[ ( tile * blockDim.x ) + i ];
 	}
 
 	shared_input[ t ] = greatest;
@@ -75,11 +75,11 @@ void kernel_setup( int*& host_array, int& output, int size ){
 
 	int array_size = size * sizeof( int );
 
-	cudaMalloc( ( void** ) &dev_array, size );
-	cudaMalloc( ( void** ) &dev_out, sizeof( int ) );
+	cudaMalloc( ( void** ) &dev_array, array_size );
+	cudaMalloc( ( void** ) &dev_out, 1 * sizeof( int ) );
 
 	cudaMemcpy( dev_array, host_array, array_size, cudaMemcpyHostToDevice );
-	cudaMemcpy( dev_out, &output, sizeof( int ), cudaMemcpyHostToDevice );
+	cudaMemcpy( dev_out, &output, 1 * sizeof( int ), cudaMemcpyHostToDevice );
 
 	dim3 blocks{ 1, 1, 1 };
 
@@ -88,11 +88,11 @@ void kernel_setup( int*& host_array, int& output, int size ){
 	std::cout << "\nEnter the Coarse Factor: ";
 	std::cin >> coarse_factor;
 
-	unsigned int num_threads{ static_cast<unsigned int>( size ) / coarse_factor };
+	unsigned int num_threads{ static_cast<unsigned int>( size ) / ( 2 * coarse_factor ) }; // Remember that atomicMax is a binary operation, i.e., involves 2 elements. Thus, 
 
 	dim3 threads{ num_threads, 1, 1 };
 
-	int shared_memsize = size * sizeof( int );
+	int shared_memsize = num_threads * sizeof( int );
 
 	CoarsenedSumReductionKernel<<<blocks, num_threads, shared_memsize>>>( dev_array, dev_out, coarse_factor );
 
