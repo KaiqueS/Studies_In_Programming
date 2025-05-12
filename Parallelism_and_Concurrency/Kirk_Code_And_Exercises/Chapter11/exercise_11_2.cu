f815f95f1cb789a33c7cf55f676fa502ae1104e7
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2025-05-12 19:12:55 -0300

    UPDATE: Exercise 11.2 - Fix rounding error

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_2.cu
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_2.cu
@@ -36,6 +36,7 @@ void print_array( double* array, int size ){
 
 /// ANSWER:
 
+// PROBLEM: as soon as N > 9, partial sums get wrong precisely at the midpoint of the array
 __global__ void Kogge_Stone_scan_kernel( double* input, double* output, unsigned int N, int offset ){
 
 	__shared__ extern double XY[ ]; // XY must have SECTION_SIZE elements, where SECTION_SIZE = Block size, i.e., amount of threads
@@ -49,7 +50,8 @@ __global__ void Kogge_Stone_scan_kernel( double* input, double* output, unsigned
 
 		__syncthreads( );
 
-		if( ( static_cast<int>( log2( static_cast<double>( stride ) ) ) ) % 2 == 0 ){
+		// NOTE: static_cast to int from double was INCORRECTLY rounding DOWN the logarithm. To solve that, I had to add 0.5.
+		if( ( static_cast<int>( log2( static_cast<double>( stride ) + 0.5 ) ) ) % 2 == 0 ){
 
 			if( threadIdx.x >= stride ){
 
@@ -58,7 +60,8 @@ __global__ void Kogge_Stone_scan_kernel( double* input, double* output, unsigned
 
 			else{
 
-				buffer[ threadIdx.x + offset ] = XY[ threadIdx.x ];
+				buffer[ threadIdx.x + offset ] = XY[ threadIdx.x ]; // NOTE: this is necessary. As iterations go on, threads with indexes SMALLER than stride will not update their required outcome elements
+																	//		 without this ELSE clause. This, in turn, makes the partial sums for subsequent elements incorrect!
 			}
 		}
 
@@ -71,14 +74,15 @@ __global__ void Kogge_Stone_scan_kernel( double* input, double* output, unsigned
 
 			else{
 
-				XY[ threadIdx.x ] = buffer[ threadIdx.x + offset ];
+				XY[ threadIdx.x ] = buffer[ threadIdx.x + offset ]; // NOTE: this is necessary. As iterations go on, threads with indexes SMALLER than stride will not update their required outcome elements
+																	//		 without this ELSE clause. This, in turn, makes the partial sums for subsequent elements incorrect!
 			}
 		}
 	}
 
 	if( i < N ){
 
-		output[ i ] = ( static_cast<int>( ceil( static_cast<double>( N ) ) ) % 2 != 0 ) ? XY[ threadIdx.x ] : buffer[ threadIdx.x + offset ];
+		output[ i ] = ( static_cast<int>( ceil( log2( static_cast<double>( N ) ) ) ) % 2 == 0 ) ? XY[ threadIdx.x ] : buffer[ threadIdx.x + offset ];
 	}
 }
 
@@ -98,7 +102,7 @@ void kernel_launch( double*& host_input, double*& host_output, int size ){
 	std::cout << "\nEnter the section size: ";
 	std::cin >> section_size;
 
-	//section_size *= sizeof( double );
+	section_size *= sizeof( double );
 
 	std::cout << "\n";
 
@@ -109,7 +113,7 @@ void kernel_launch( double*& host_input, double*& host_output, int size ){
 
 	std::cout << "\n";
 
-	dim3 blocks{ 1 }; // TODO: fix this! This is incorrect. The correct amount of block should be input size / section size
+	dim3 blocks{ 1 }; // TODO: fix this! This is incorrect. The correct amount of blocks should be input size / section size
 	dim3 threads{ num_of_threads };
 
 	Kogge_Stone_scan_kernel<<<blocks, threads, section_size>>>( dev_input, dev_output, size, section_size );
