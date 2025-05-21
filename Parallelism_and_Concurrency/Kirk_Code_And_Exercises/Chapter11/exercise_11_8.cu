9d315b330f23196fef9b6882cb52978d7cd04f3f
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2025-05-20 22:12:12 -0300

    UPDATE: Exercise 11.8 - Kernels 2 and 3 START

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_8.cu
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_8.cu
@@ -38,7 +38,7 @@ void print_array( double*& array, int size ){
 // NOTE: I will be using the Brent-Kung Algorithm here.
 // PROBLEM: when the size of the input array = 6, the last prefix sum is wrong
 //			Seems like a rounding error when dividing Section_Size / 4
-__global__ void First_Kernel( double* input, double* output, unsigned int input_size, unsigned int Section_Size ){
+__global__ void First_Kernel( double* input, double* output, double* S, unsigned int input_size, unsigned int Section_Size ){
 
 	__shared__ extern double Shared_Input[ ];
 
@@ -47,7 +47,6 @@ __global__ void First_Kernel( double* input, double* output, unsigned int input_
 	if( i < input_size ){
 
 		Shared_Input[ threadIdx.x ] = input[ i ];
-		//Shared_Input[ threadIdx.x + ( input_size / 2 ) ] = input[ i + ( input_size / 2 ) ];
 	}
 
 	if( ( i + blockDim.x ) < input_size ){
@@ -70,7 +69,83 @@ __global__ void First_Kernel( double* input, double* output, unsigned int input_
 	int correct_rounding{ 0 };
 
 	// NOTE: this is ugly, but necessary. The example code on the book is wrong. The code output is wrong/right depending on how Section_Size / 4 is rounded by the compiler.
-	//		 The rule below captures all cases and correctly rounds the quocient.
+	//		 The rule below captures all cases and correctly rounds the quocient. The idea is - if rounding the quocient up results in an even ceiling, keep it, else, return
+	//		 the floor of the quocient.
+	if( Section_Size < 4 ){
+
+		correct_rounding = static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) );
+	}
+
+	else{
+
+		correct_rounding = ( static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) ) % 2 == 0 ) ? static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) ) :
+																												static_cast<int>( floor( static_cast<double>( Section_Size ) / 4.0 ) );
+	}
+
+	for( int stride = correct_rounding; stride > 0; stride /= 2 ){
+
+		__syncthreads( );
+
+		unsigned int index = ( ( threadIdx.x + 1 ) * stride * 2 ) - 1;
+
+		if( ( index + stride ) < Section_Size ){
+
+			//printf( "%d %d %d %f %f\n", threadIdx.x, ( index + stride ), stride, Shared_Input[ index + stride ], Shared_Input[ index ] );
+
+			Shared_Input[ index + stride ] += Shared_Input[ index ];
+		}
+	}
+
+	__syncthreads( );
+
+	if( i < input_size ){
+
+		output[ i ] = Shared_Input[ threadIdx.x ];
+	}
+
+	if( ( i + blockDim.x ) < input_size ){
+
+		output[ i + blockDim.x ] = Shared_Input[ threadIdx.x + blockDim.x ];
+	}
+
+	__syncthreads( );
+
+	S[ blockIdx.x ] = ( threadIdx.x == ( blockDim.x - 1 ) ) ? Shared_Input[ Section_Size - 1 ] : 0.0;
+}
+
+__global__ void Second_Kernel( double* S, unsigned int input_size, unsigned int Section_Size ){
+
+	__shared__ extern double Shared_Input[ ];
+
+	unsigned int i = ( 2 * blockIdx.x * blockDim.x ) + threadIdx.x;
+
+	if( i < input_size ){
+
+		Shared_Input[ threadIdx.x ] = S[ i ];
+	}
+
+	if( ( i + blockDim.x ) < input_size ){
+
+		Shared_Input[ threadIdx.x + blockDim.x ] = S[ i + blockDim.x ];
+	}
+
+	for( unsigned int stride = 1; stride <= blockDim.x; stride *= 2 ){
+
+		__syncthreads( );
+
+		unsigned int index = ( ( threadIdx.x + 1 ) * ( 2 * stride ) ) - 1;
+
+		if( index < Section_Size ){
+
+			Shared_Input[ index ] += Shared_Input[ index -  stride ];
+		}
+	}
+
+	int correct_rounding{ 0 };
+
+	// NOTE: this is ugly, but necessary. The example code on the book is wrong. The code output is wrong/right depending on how Section_Size / 4 is rounded by the compiler.
+	//		 The rule below captures all cases and correctly rounds the quocient. The idea is - if rounding the quocient up results in an even ceiling, keep it, else, return
+	//		 the floor of the quocient.
 	if( Section_Size < 4 ){
 
 		correct_rounding = static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) );
@@ -98,29 +173,25 @@ __global__ void First_Kernel( double* input, double* output, unsigned int input_
 
 	if( i < input_size ){
 
-		output[ i ] = Shared_Input[ threadIdx.x ];
+		S[ i ] = Shared_Input[ threadIdx.x ];
 	}
 
 	if( ( i + blockDim.x ) < input_size ){
 
-		output[ i + blockDim.x ] = Shared_Input[ threadIdx.x + blockDim.x ];
-		//output[ i + blockDim.x + ( input_size / 2 ) ] = Shared_Input[ threadIdx.x + blockDim.x + ( input_size / 2 ) ];
+		S[ i + blockDim.x ] = Shared_Input[ threadIdx.x + blockDim.x ];
 	}
 }
 
-__global__ void Second_Kernel( ){
+__global__ void Third_Kernel( double* S, double* output ){
 
+	unsigned int i = ( blockIdx.x * blockDim.x ) + threadIdx.x;
 
-}
-
-__global__ void Third_Kernel( ){
-
-
+	output[ i ] += S[ blockIdx.x - 1 ];
 }
 
 void kernel_setup( double*& host_input, double*& host_output, unsigned int size ){
 
-	double* dev_in{ nullptr }, *dev_out{ nullptr };
+	double* dev_in{ nullptr }, *dev_out{ nullptr }, *dev_S{ nullptr };
 
 	unsigned int array_size = size * sizeof( double );
 
@@ -129,10 +200,19 @@ void kernel_setup( double*& host_input, double*& host_output, unsigned int size 
 
 	cudaMemcpy( dev_in, host_input, array_size, cudaMemcpyHostToDevice );
 
-	dim3 blocks{ 1, 1, 1 };
+	unsigned int num_of_blocks{ 0 };
+
+	std::cout << "\nEnter the amount of blocks: ";
+	std::cin >> num_of_blocks;
+
+	dim3 blocks{ num_of_blocks, 1, 1 };
 	dim3 threads{ static_cast<unsigned int>( std::ceil( ( static_cast<double>( size ) / 2.0 ) ) ) };
 
-	First_Kernel<<<blocks, threads, array_size>>>( dev_in, dev_out, size, size );
+	unsigned int Section_Size = static_cast<unsigned int>( std::ceil( static_cast<double>( size / num_of_blocks ) ) );
+
+	cudaMalloc( ( void** ) &dev_S, ( num_of_blocks * sizeof( double ) ) );
+
+	First_Kernel<<<blocks, threads, Section_Size>>>( dev_in, dev_out, dev_S, size, Section_Size );
 
 	cudaMemcpy( host_output, dev_out, array_size, cudaMemcpyDeviceToHost );
 
@@ -156,9 +236,11 @@ int main( ){
 
 	kernel_setup( array, output, size );
 
-	std::cout << "\n";
+	std::cout << "\n\n";
 
 	print_array( output, size );
+
+	// TODO: find Shared Memory maximum size. Use it to determine the amount of blocks needed
 
 	delete[ ] array, output;
 }
