66bb5bbb646fc0fa91cae838db5db87231c32175
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2025-05-23 20:34:51 -0300

    UPDATE: Exercise 11.8 - Really fixed the indexing error. ALMOST THERE

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_8.cu
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_8.cu
@@ -35,9 +35,10 @@ void print_array( double*& array, int size ){
 
 /// ANSWER:
 
-// NOTE: I will be using the Brent-Kung Algorithm here.
+// NOTE: I am using the Brent-Kung Algorithm here.
 // PROBLEM: when the size of the input array = 6, the last prefix sum is wrong
 //			Seems like a rounding error when dividing Section_Size / 4
+// SOLVED: check EXPLANATION
 __global__ void First_Kernel( double* input, double* output, double* S, unsigned int input_size, unsigned int Section_Size ){
 
 	__shared__ extern double Shared_Input[ ];
@@ -54,15 +55,16 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 		Shared_Input[ threadIdx.x + blockDim.x ] = input[ i + blockDim.x ];
 	}
 
+	// This is wrong. Somehow, we are leaving the for loop without the correct complete sum on the last element of shared_input
 	for( unsigned int stride = 1; stride <= blockDim.x; stride *= 2 ){
 
 		__syncthreads( );
 
-		unsigned int index = ( ( threadIdx.x + 1 ) * ( 2 * stride ) ) - 1;
+		unsigned int index = ( ( threadIdx.x + 1 ) * 2 * stride ) - 1;
 
 		if( index < Section_Size ){
 
-			Shared_Input[ index ] += Shared_Input[ index -  stride ];
+			Shared_Input[ index ] += Shared_Input[ index - stride ];
 		}
 	}
 
@@ -70,8 +72,8 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 
 	// NOTE: this is ugly, but necessary. The example code on the book is wrong. The code output is wrong/right depending on how Section_Size / 4 is rounded by the compiler.
 	//		 The rule below captures all cases and correctly rounds the quocient. The idea is - if rounding the quocient up results in an even ceiling, keep it, else, return
-	//		 the floor of the quocient.
-	if( Section_Size < 4 ){
+	//		 the floor of the quocient. -> WRONG. Check the correct index below
+	/*if( Section_Size < 4 ){
 
 		correct_rounding = static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) );
 	}
@@ -80,7 +82,11 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 
 		correct_rounding = ( static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) ) % 2 == 0 ) ? static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) ) :
 																												static_cast<int>( floor( static_cast<double>( Section_Size ) / 4.0 ) );
-	}
+	}*/
+
+	// EXPLANATION: stride must be in the form of Powers of 2, but the code on the book allows for values that are not powers of 2. Thus, we pick the smallest power of 2 that is
+	//				greater than the quocient Section_Size / 4.
+	correct_rounding = static_cast<int>( pow( 2.0, ceil( log2( static_cast<double>( Section_Size ) / 4.0 ) ) ) );
 
 	for( int stride = correct_rounding; stride > 0; stride /= 2 ){
 
@@ -88,9 +94,13 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 
 		unsigned int index = ( ( threadIdx.x + 1 ) * stride * 2 ) - 1;
 
+		// TEST: for input_size = 24, no matter threadIdx and stride, index + stride NEVER hits 23. Thus, the last element in Shared_Input is never hit
 		if( ( index + stride ) < Section_Size ){
 
-			//printf( "%d %d %d %f %f\n", threadIdx.x, ( index + stride ), stride, Shared_Input[ index + stride ], Shared_Input[ index ] );
+			if( index == 5 ){
+
+				printf( "\n%d %d %d %f %f", threadIdx.x, ( index + stride ), stride, Shared_Input[ index + stride ], Shared_Input[ index ] );
+			}
 
 			Shared_Input[ index + stride ] += Shared_Input[ index ];
 		}
@@ -143,19 +153,7 @@ __global__ void Second_Kernel( double* S, unsigned int input_size, unsigned int 
 
 	int correct_rounding{ 0 };
 
-	// NOTE: this is ugly, but necessary. The example code on the book is wrong. The code output is wrong/right depending on how Section_Size / 4 is rounded by the compiler.
-	//		 The rule below captures all cases and correctly rounds the quocient. The idea is - if rounding the quocient up results in an even ceiling, keep it, else, return
-	//		 the floor of the quocient.
-	if( Section_Size < 4 ){
-
-		correct_rounding = static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) );
-	}
-
-	else{
-
-		correct_rounding = ( static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) ) % 2 == 0 ) ? static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) ) :
-																												static_cast<int>( floor( static_cast<double>( Section_Size ) / 4.0 ) );
-	}
+	correct_rounding = static_cast<int>( pow( 2.0, ceil( log2( static_cast<double>( Section_Size ) / 4.0 ) ) ) );
 
 	for( int stride = correct_rounding; stride > 0; stride /= 2 ){
 
