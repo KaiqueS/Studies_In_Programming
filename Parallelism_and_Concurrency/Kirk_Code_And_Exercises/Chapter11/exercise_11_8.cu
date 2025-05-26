92c9b61ed99730a0ab5e5c94f7292c9425afb259
Kaique Santos <14958834+KaiqueS@users.noreply.github.com>
2025-05-26 19:14:31 -0300

    UPDATE: Exercise 11.8 - END

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_8.cu
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter11/exercise_11_8.cu
@@ -71,8 +71,8 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 	int correct_rounding{ 0 };
 
 	// NOTE: this is ugly, but necessary. The example code on the book is wrong. The code output is wrong/right depending on how Section_Size / 4 is rounded by the compiler.
-	//		 The rule below captures all cases and correctly rounds the quocient. The idea is - if rounding the quocient up results in an even ceiling, keep it, else, return
-	//		 the floor of the quocient. -> WRONG. Check the correct index below
+	//		 The rule below captures all cases and correctly rounds the quotient. The idea is - if rounding the quotient up results in an even ceiling, keep it, else, return
+	//		 the floor of the quotient. -> WRONG. Check the correct index below
 	/*if( Section_Size < 4 ){
 
 		correct_rounding = static_cast<int>( ceil( static_cast<double>( Section_Size ) / 4.0 ) );
@@ -85,7 +85,7 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 	}*/
 
 	// EXPLANATION: stride must be in the form of Powers of 2, but the code on the book allows for values that are not powers of 2. Thus, we pick the smallest power of 2 that is
-	//				greater than the quocient Section_Size / 4.
+	//				greater than the quotient Section_Size / 4.
 	correct_rounding = static_cast<int>( pow( 2.0, ceil( log2( static_cast<double>( Section_Size ) / 4.0 ) ) ) );
 
 	for( int stride = correct_rounding; stride > 0; stride /= 2 ){
@@ -97,11 +97,6 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 		// TEST: for input_size = 24, no matter threadIdx and stride, index + stride NEVER hits 23. Thus, the last element in Shared_Input is never hit
 		if( ( index + stride ) < Section_Size ){
 
-			if( index == 5 ){
-
-				printf( "\n%d %d %d %f %f", threadIdx.x, ( index + stride ), stride, Shared_Input[ index + stride ], Shared_Input[ index ] );
-			}
-
 			Shared_Input[ index + stride ] += Shared_Input[ index ];
 		}
 	}
@@ -110,19 +105,30 @@ __global__ void First_Kernel( double* input, double* output, double* S, unsigned
 
 	if( i < input_size ){
 
-		output[ i ] = Shared_Input[ threadIdx.x ];
+		double holder = Shared_Input[ threadIdx.x ];
+
+		output[ i ] = holder;
 	}
 
 	if( ( i + blockDim.x ) < input_size ){
 
-		output[ i + blockDim.x ] = Shared_Input[ threadIdx.x + blockDim.x ];
+		double holder = Shared_Input[ threadIdx.x + blockDim.x ];
+
+		output[ i + blockDim.x ] = holder;
 	}
 
 	__syncthreads( );
 
-	S[ blockIdx.x ] = ( threadIdx.x == ( blockDim.x - 1 ) ) ? Shared_Input[ Section_Size - 1 ] : 0.0;
+	if( threadIdx.x == blockDim.x - 1 ){
+
+		// NOTE: remember that Shared_Input is in shared-memory, which is BLOCK-LOCAL! I.e., e.g., blocks 1 and 2 do NOT share shared-memory address spaces!
+		S[ blockIdx.x ] = Shared_Input[ Section_Size - 1 ];
+	}
 }
 
+// NOTE: Shared Memory PERSISTS BETWEEN KERNEL CALLS! Keep that in mind, because, unless explicitly modified, values from previous kernel calls might still be
+//		 in Shared Memory when you run other kernels that use it. But, since Shared Memory is block-local, I do not know what addresses intervals will be assigned
+//		 to blocks from different kernels.
 __global__ void Second_Kernel( double* S, unsigned int input_size, unsigned int Section_Size ){
 
 	__shared__ extern double Shared_Input[ ];
@@ -180,11 +186,20 @@ __global__ void Second_Kernel( double* S, unsigned int input_size, unsigned int 
 	}
 }
 
+// NOTE: an interesting problem happened here when testing the code - To check if output was correct, I let the first GLOBAL thread, taken from 2 different blocks, print all output elements.
+//		 The thing is, output is not modified by Second_Kernel, only by the First. This means that the printed values should match the values output has at the end of the First_Kernel call.
+//		 But they did NOT match. Why? Because, the first GLOBAL thread, which is in the first block, printed values only AFTER the threads from the SECOND block modified output, which lies in
+//		 global memory.
 __global__ void Third_Kernel( double* S, double* output ){
 
 	unsigned int i = ( blockIdx.x * blockDim.x ) + threadIdx.x;
 
-	output[ i ] += S[ blockIdx.x - 1 ];
+	// EXPLANATION: all the partial prefix sums from the first section, which was assigned to the first block, are already correct. Thus, there is no need to
+	//				add to them any value from S.
+	if( blockIdx.x > 0 ){
+
+		output[ i ] += S[ blockIdx.x - 1 ];
+	}
 }
 
 void kernel_setup( double*& host_input, double*& host_output, unsigned int size ){
@@ -193,29 +208,33 @@ void kernel_setup( double*& host_input, double*& host_output, unsigned int size 
 
 	unsigned int array_size = size * sizeof( double );
 
-	cudaMalloc( ( void** ) &dev_in, array_size );
-	cudaMalloc( ( void** ) &dev_out, array_size );
-
-	cudaMemcpy( dev_in, host_input, array_size, cudaMemcpyHostToDevice );
-
 	unsigned int num_of_blocks{ 0 };
 
 	std::cout << "\nEnter the amount of blocks: ";
 	std::cin >> num_of_blocks;
 
+	cudaMalloc( ( void** ) &dev_in, array_size );
+	cudaMalloc( ( void** ) &dev_out, array_size );
+	cudaMalloc( ( void** ) &dev_S, ( num_of_blocks * sizeof( double ) ) );
+
+	cudaMemcpy( dev_in, host_input, array_size, cudaMemcpyHostToDevice );
+
 	dim3 blocks{ num_of_blocks, 1, 1 };
-	dim3 threads{ static_cast<unsigned int>( std::ceil( ( static_cast<double>( size ) / 2.0 ) ) ) };
+	dim3 threads{ static_cast<unsigned int>( std::ceil( ( static_cast<double>( size ) / ( 2.0 * static_cast<double>( num_of_blocks ) ) ) ) ) };
 
 	unsigned int Section_Size = static_cast<unsigned int>( std::ceil( static_cast<double>( size / num_of_blocks ) ) );
 
-	cudaMalloc( ( void** ) &dev_S, ( num_of_blocks * sizeof( double ) ) );
-
 	First_Kernel<<<blocks, threads, Section_Size>>>( dev_in, dev_out, dev_S, size, Section_Size );
+
+	Second_Kernel<<<1, blocks, num_of_blocks>>>( dev_S, num_of_blocks, num_of_blocks );
+
+	Third_Kernel<<<blocks, Section_Size>>>( dev_S, dev_out );
 
 	cudaMemcpy( host_output, dev_out, array_size, cudaMemcpyDeviceToHost );
 
 	cudaFree( dev_in );
 	cudaFree( dev_out );
+	cudaFree( dev_S );
 }
 
 int main( ){
