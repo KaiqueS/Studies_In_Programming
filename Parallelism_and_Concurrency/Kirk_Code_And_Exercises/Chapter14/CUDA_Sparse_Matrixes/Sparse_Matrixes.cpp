a0d038de907ffb5eed84ce92cf46dfe20a57f390
Kaique P. Santos <14958834+KaiqueS@users.noreply.github.com>
2026-01-06 21:06:50 -0300

    UPDATE: Finished COO

--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter14/CUDA_Sparse_Matrixes/Sparse_Matrixes.cpp
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter14/CUDA_Sparse_Matrixes/Sparse_Matrixes.cpp
@@ -3,6 +3,72 @@
 /// COO - BEGIN
 
 
+// NOTE: I could parallelize this later using tiling to speed things up.
+int COO::count_Zeroes( int**& matrix, int rowSize, int colSize ){
+
+	int counter{ 0 };
+
+	for( auto i = 0; i < rowSize; ++i ){
+
+		for( auto j = 0; j < colSize; ++j ){
+
+			if( matrix[ i ][ j ] == 0 ){
+
+				++counter;
+			}
+
+			else{
+
+				continue;
+			}
+		}
+	}
+
+	return counter;
+}
+
+// NOTE: COO arrays are unidimensional
+void COO::allocate_COO( int**&matrix, int rowSize, int colSize ){
+
+	int zeroes{ count_Zeroes( matrix, rowSize, colSize ) };
+
+	rowIdx.reserve( ( rowSize * colSize ) - zeroes );
+	colIdx.reserve( ( rowSize * colSize ) - zeroes );
+	value.reserve( ( rowSize * colSize ) - zeroes );
+}
+
+void COO::build_COO( int**& matrix, int rowSize, int colSize ){
+
+	allocate_COO( matrix, rowSize, colSize );
+
+	for( auto i = 0; i < rowSize; ++i ){
+
+		for( auto j = 0; j < colSize; ++j ){
+
+			// NOTE: any time we hit a 0, we use zeroes to put matrix[ i ][ j ] into the correct position in value, rowIdx, and colIdx
+			if( matrix[ i ][ j ] != 0 ){
+
+				value.push_back( matrix[ i ][ j ] );
+				rowIdx.push_back( i );
+				colIdx.push_back( j );
+			}
+			
+			// NOTE: If we hit a 0 in matrix, we increment the offset zeroes
+			else{
+
+				continue;
+			}
+		}
+	}
+}
+
+void COO::insert_element( int row, int col, int element ){
+
+	rowIdx.push_back( row );
+	colIdx.push_back( col );
+	value.push_back( element );
+}
+
 /// END - COO
 
 // ------------------
--- Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter14/CUDA_Sparse_Matrixes/Sparse_Matrixes.hpp
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter14/CUDA_Sparse_Matrixes/Sparse_Matrixes.hpp
@@ -3,23 +3,31 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <stdio.h>
-#include <iostream>
-#include <random>
+#include <vector>
 
+// NOTE: maybe I should have used std::vector or unique_ptr, instead of raw arrays?
 class COO{
 
 public:
 
 	COO( ){ }
+	~COO( ){ rowIdx.clear( ); colIdx.clear( ); value.clear( ); }
 
+	void allocate_COO( int**&matrix, int rowSize, int colSize );
+	void build_COO( int**& matrix, int rowSize, int colSize );
+	void insert_element( int row, int col, int element );
 
+	int count_Zeroes( int**& matrix, int rowSize, int colSize );
+
+	const std::vector<int>& get_rowIdx( ) const{ return rowIdx; }
+	const std::vector<int>& get_colIdx( ) const{ return colIdx; }
+	const std::vector<int>& get_value( ) const{ return value; }
 
 private:
 
-	int* rowIdx{ nullptr };
-	int* colIdx{ nullptr };
-	int* value{ nullptr };
+	std::vector<int> rowIdx{ };
+	std::vector<int> colIdx{ };
+	std::vector<int> value{ };
 };
 
 class CSR{
@@ -33,6 +41,16 @@ private:
 };
 
 class ELL{
+
+public:
+
+
+private:
+
+
+};
+
+class JDS{
 
 public:
 
