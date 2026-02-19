ee5fd5466e52b544598867800a0a2bf9b62a6705
Kaíque P. Santos <kaiquepsantos@protonmail.com>
2026-02-18 21:30:02 -0400

    UPLOAD: Exercise 14.4 - START

---
+++ Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter14/exercise_14_4.cu
@@ -0,0 +1,19 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "/Projetos/Studies_In_Programming/Parallelism_and_Concurrency/Kirk_Code_And_Exercises/Chapter14/CUDA_Sparse_Matrixes/Sparse_Arrays.hpp"
+
+#include <stdio.h>
+
+/// PROBLEM 4. Implement the host code for producing the hybrid ELL-COO format and using it to perform SpMV.
+///            Launch the ELL kernel to execute on the device, and compute the contributions of the COO elements on the host.
+
+void kernel_setup( ){
+
+
+}
+
+int main( ){
+
+
+}
